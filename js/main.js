e1ad5362e1ebf648ed5f09d0fd7c81acbbacb4ab
Alexey Alexeew <lelikboss97@gmail.com>
2021-08-23 17:02:30 +0300

    all change and finish work

--- js/main.js
+++ js/main.js
@@ -1,76 +1,46 @@
-// const refs = {
-//   days: document.querySelector("[data-value='days']"),
-//   hours: document.querySelector("[data-value=hours]"),
-//   min: document.querySelector("[data-value=mins]"),
-//   sec: document.querySelector("[data-value=secs]"),
-// };
-// console.log(refs.days.textContent);
-
-// let targetDate = new Date("Aug 20, 2021");
-// console.log(targetDate.getDate());
-// const today = new Date();
-// console.log(today);
-// let watchId = null;
-// function timer() {
-//   const time = targetDate - today;
-//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
-//   refs.days.textContent = days;
-//   refs.hours.textContent = hours;
-//   refs.min.textContent = mins;
-//   refs.sec.textContent = secs;
-//   startWatch();
-// }
-// function startWatch() {
-//   targetDate = new Date("Aug 20, 2021");
-//   watchId = setInterval(timer, 1000);
-//   console.log(watchId);
-// }
-
-// window.addEventListener("DOMContentLoaded", startWatch);
-//!
-const refs = {
-  timer: document.querySelector("#timer-1"),
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
-};
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.targetDate = targetDate;
+    this.refs = {
+      timer: document.querySelector("#timer-1"),
+      days: document.querySelector('[data-value="days"]'),
+      hours: document.querySelector('[data-value="hours"]'),
+      mins: document.querySelector('[data-value="mins"]'),
+      secs: document.querySelector('[data-value="secs"]'),
+    };
     this.selector = selector;
+    this.targetDate = targetDate;
   }
 
   timerId = setInterval(() => {
-    let today = Date.now();
-    let timerDate = this.targetDate - today;
-    changeTime(timerDate);
-    stopInterval(timerDate);
+    const today = Date.now();
+    let time = this.targetDate - today;
+    this.changeTime(time);
+    this.finishTimer(time);
   }, 1000);
-}
 
-function changeTime(time) {
-  // const time = this.targetDate - this.today;
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  refs.days.textContent = days;
-  refs.hours.textContent = hours;
-  refs.mins.textContent = mins;
-  refs.secs.textContent = secs;
-}
-function stopInterval(timerDate) {
-  if (timerDate < 0) {
-    clearInterval(this.timerId);
-    alert("GO!");
+  changeTime(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    this.refs.days.textContent = days < 10 ? `0${days}` : days;
+    this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+    this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
+    this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
+  }
+
+  finishTimer(time) {
+    if (time <= 0) {
+      clearInterval(this.timerId);
+      this.refs.days.textContent = "00";
+      this.refs.hours.textContent = "00";
+      this.refs.mins.textContent = "00";
+      this.refs.secs.textContent = "00";
+    }
   }
 }
+
 new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Aug 30, 2021 15:52:00"),
+  targetDate: new Date("Dec 31, 2021 23:59:59"),
 });
-//!
